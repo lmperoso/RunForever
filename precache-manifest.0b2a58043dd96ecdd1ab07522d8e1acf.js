7df41896d861a6e55d50cf1f31a76036b016e869
lmperoso <catxalote@hotmail.com>
2020-06-11 21:15:06 +0200

    RunForever

---
+++ precache-manifest.0b2a58043dd96ecdd1ab07522d8e1acf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4c45b4c4ab6d1330a5",
+    "url": "/css/app.84c65ef4.css"
+  },
+  {
+    "revision": "189a4b238a661c6b7cdd",
+    "url": "/css/chunk-vendors.84976132.css"
+  },
+  {
+    "revision": "4a6542eba6a5a585631a51074e6c6760",
+    "url": "/img/logo.4a6542eb.png"
+  },
+  {
+    "revision": "8c9c92ef515721314d78d8265276285c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4c45b4c4ab6d1330a5",
+    "url": "/js/app.ba258e98.js"
+  },
+  {
+    "revision": "189a4b238a661c6b7cdd",
+    "url": "/js/chunk-vendors.069e8e26.js"
+  },
+  {
+    "revision": "2f8b0a6d94c12935d3a07eb6ac937f09",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
